01a67afe708a7dd4a7be8985d4292104c313426b
sivaprasad272 <99789786+sivaprasad272@users.noreply.github.com>
2023-07-10 22:14:20 +0530

    Create pushimage.sh

---
+++ pushimage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Authenticate with the container registry using Docker CLI
+docker login -u sivaprasad272 -p prasad272@  gitubuntu
+
+# Push the Docker image to the registry
+docker push gitubnutu
