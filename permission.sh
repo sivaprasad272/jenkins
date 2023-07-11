0109001dda033b3e4573352b2249f22233a5fc90
sivaprasad272 <99789786+sivaprasad272@users.noreply.github.com>
2023-07-11 11:30:29 +0530

    Update permission.sh

--- permission.sh
+++ permission.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Grant sudoers permission to the current user without password prompt
-echo "$jenkins ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$jenkins
-sudo chmod 0440 /etc/sudoers.d/$jenkins
+sudo usermod -aG sudo username
 
-# Run the rest of your commands that require sudo
-# ...
