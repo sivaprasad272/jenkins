cb3da4eb1927b045c8683c77c65db875522c705c
sivaprasad272 <99789786+sivaprasad272@users.noreply.github.com>
2023-07-11 09:11:59 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,13 @@
+
+# Install Docker
+sudo -n yum update -y
+
+
+# Install Docker
+#sudo yum install -y docker
+
+# Start Docker service
+#sudo service docker start
+
+# Add current user to the docker group
+#sudo usermod -a -G docker $USER
