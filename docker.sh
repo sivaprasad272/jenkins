8f48a530976e1b369d058542306f5b4343d342ac
sivaprasad272 <99789786+sivaprasad272@users.noreply.github.com>
2023-07-11 22:35:44 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,13 +1,6 @@
 
-# Install Docker
-sudo -n yum update -y
-
-
-# Install Docker
-#sudo yum install -y docker
-
-# Start Docker service
-#sudo service docker start
 
 # Add current user to the docker group
 sudo usermod -a -G docker $jenkins
+
+sudo systemctl restart docker
